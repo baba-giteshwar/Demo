209f28ca9e599e2f43b83f06ee24c1af1d985206
baba-giteshwar <132001682+baba-giteshwar@users.noreply.github.com>
2023-04-29 13:28:33 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://demoqa.com',
-    screenshotsFolder: 'cypress/screenshots',
     chromeWebSecurity: false,
-    "trashAssetsBeforeHeadlessRuns": false,
-    reporter: 'junit',
-    reporterOptions: {
-      mochaFile: 'results/my-test-output.xml',
-      toConsole: true,
-  },
-    setupNodeEvents(on, config) {
-      
-    },
-    
-  },
+    "video": false,
+    "screenshotOnRunFailure": false,
+    "trashAssetsBeforeHeadlessRuns": false, 
+  }
   
 });
